b03a41269c0ba499b412d4da409b9b3444da51c6
AdrienMany <manyadrien@gmail.com>
2019-09-16 17:05:53 +0200

    setup routes

--- smashhowtobeat/src/App.js
+++ smashhowtobeat/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>Smash Ultimate how to beat</h1>
     </div>
+    </BrowserRouter>
   );
 }
 
