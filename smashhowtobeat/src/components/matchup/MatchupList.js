46959d9c01f13d80b40c47cefbd576d36f438c5f
AdrienMany <manyadrien@gmail.com>
2019-09-16 18:04:53 +0200

    matchup summary component

--- smashhowtobeat/src/components/matchup/MatchupList.js
+++ smashhowtobeat/src/components/matchup/MatchupList.js
@@ -1,32 +1,13 @@
 import React from 'react';
+import MatchupSummary from './MatchupSummary';
 
 const MatchupList = () => {
     return (
         <div className="matchup-list section">
 
-            <div className="matchup-summary card z-depths-0">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Matchup title</span>
-                    <p>Posted by AdrienMany</p>
-                    <p className="grey-text">Date</p>
-                </div>
-            </div>
-
-            <div className="matchup-summary card z-depths-0">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Matchup title</span>
-                    <p>Posted by AdrienMany</p>
-                    <p className="grey-text">Date</p>
-                </div>
-            </div>
-
-            <div className="matchup-summary card z-depths-0">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Matchup title</span>
-                    <p>Posted by AdrienMany</p>
-                    <p className="grey-text">Date</p>
-                </div>
-            </div>
+            <MatchupSummary></MatchupSummary>
+            <MatchupSummary></MatchupSummary>
+            <MatchupSummary></MatchupSummary>
 
         </div>
     )
---
+++ smashhowtobeat/src/components/matchup/MatchupSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MatchupSummary = () => {
+    return (
+        <div className="matchup-summary card z-depths-0">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Matchup title</span>
+                <p>Posted by AdrienMany</p>
+                <p className="grey-text">Date</p>
+            </div>
+        </div>
+    )
+}
+
+export default MatchupSummary;
