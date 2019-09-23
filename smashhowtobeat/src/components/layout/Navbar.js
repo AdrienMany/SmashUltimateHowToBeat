65b7cc57601e4389a8be2be36a6ad1721b4d56a5
AdrienMany <manyadrien@gmail.com>
2019-09-23 18:02:32 +0200

    sign out

--- smashhowtobeat/src/components/layout/Navbar.js
+++ smashhowtobeat/src/components/layout/Navbar.js
@@ -4,22 +4,22 @@ import SignIn from './SignIn';
 import SignOut from './SignOut';
 import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const isLogged = props.auth.uid ? <SignIn></SignIn> : <SignOut></SignOut>;
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to="/" className="brand-logo">Smash Matchup</Link>
-                <SignIn></SignIn>
-                <SignOut></SignOut>
+                { isLogged }
             </div>
         </nav>
     );
 }
 
 const mapStateToProps = (state) => {
-    console.log(state);
+    // console.log(state);
     return {
-
+        auth: state.firebase.auth
     }
 }
 
--- smashhowtobeat/src/components/layout/SignIn.js
+++ smashhowtobeat/src/components/layout/SignIn.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut } from '../../store/actions/authActions';
 
-const SignIn = () => {
+const SignIn = (props) => {
     return (
         <ul className="right">
             <li><NavLink to='/newmatchup'>New Matchup</NavLink></li>
-            <li><NavLink to='/'>Log Out</NavLink></li>
+            <li><a onClick={props.signOut}>Log Out</a></li>
             <li><NavLink to='/' className="btn btn-floating pink lighten-1">AM</NavLink></li>
         </ul>
     );
 }
 
-export default SignIn;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(SignIn);
